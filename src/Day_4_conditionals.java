ed6b2440814acfbacf638e616abbdf0dbc409995
abhiramreddymada <abhiramreddy5510@gmail.com>
2025-01-06 12:40:43 +0530

    firstcommit

---
+++ src/Day_4_conditionals.java
@@ -0,0 +1,19 @@
+public class Day_4_conditionals {
+    public static void main(String[] args){
+        public boolean cigarParty(int cigars, boolean isWeekend) {
+            if(isWeekend){
+              if(cigars>=40){
+                return true;
+              }else{
+                return false;
+              }
+            }else{
+              if(cigars>=40&&cigars<=60){
+                return true;
+              }else{
+                return false;
+              }
+            }
+          }
+    }
+}
